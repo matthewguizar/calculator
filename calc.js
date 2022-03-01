fb7972e95bc43e149f211a0eade8e854e13dea61
Matthew Guizar <matthewrguizar@gmail.com>
2022-03-01 14:29:29 -0800

    almost finished

--- calc.js
+++ calc.js
@@ -4,9 +4,6 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-
-
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
@@ -14,33 +11,31 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
-	console.log('mathSymbol', mathSymbol);
-	console.log('num1', num1);
-	console.log('num2', num2);
-
 	if (mathSymbol === "+"){
 		console.log(num1 + num2);
 	}	
-	if (mathSymbol === "-"){
+	else if (mathSymbol === "-"){
 		console.log(num1 - num2);
 	}
-	if (mathSymbol === "*"){
+	else if (mathSymbol === "*"){
 		console.log(num1 * num2);
 	}
-	if (mathSymbol === "/"){
+	else if (mathSymbol === "/"){
 		console.log(num1 / num2);
 	}
-	if (mathSymbol === "Sq()"){
-		console.log(math.sqrt() (num1,num2));
+	else if (mathSymbol === "root"){
+		console.log(Math.sqrt(num1));
 	}
-	if (mathSymbol === "(num1^num2"){
-		console.log(math.pow() (numb1, numb2));
+	else if (mathSymbol === "^"){
+		console.log(Math.pow(num1, num2));
 	}
-	if (mathSymbol === "%"){
-		console.log(num1 % num2);
-	}
+	else if (mathSymbol === "%"){
+		console.log(Math.sqrt(num1));
 
+	} else { 
+		console.log("Please Try Again");
+	}
 	// This line closes the connection to the command line interface.
-	reader.close()
+	reader.close();
 
 });
